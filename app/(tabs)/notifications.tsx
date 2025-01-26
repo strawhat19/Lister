60960c002ef8edfc0844ad353903204f7ccc1977
rakib <rahmed@hvac.mea.com>
2025-01-26 03:26:05 -0500

    Without Dynamic

--- app/(tabs)/notifications.tsx
+++ app/(tabs)/notifications.tsx
@@ -1,13 +1,12 @@
-// import Sortable from '@/components/board/list/sortable';
-// import Example from '@/components/board/list/example/example';
-import Reorderable from '@/components/board/list/reordorable';
 // import Snack from '@/components/board/list/snack';
+// import Sortable from '@/components/board/list/sortable';
+import Reorderable from '@/components/board/list/reordorable';
+// import Example from '@/components/board/list/example/example';
 // import FlatListExample from '@/components/board/slider/slide/flatlist-example';
 
 export default function Notifications() {
     return (
         // <FlatListExample />
-        // <Example />
         // <Snack />
         <Reorderable />
         // <Sortable />
